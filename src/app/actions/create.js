2db560b7b88486b64e260ce2d72404797365063f
RyanSikandar <ryansikandar@gmail.com>
2025-02-11 19:49:07 +0500

    feat: redis pipeline and get hash vals

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -1,13 +1,25 @@
 'use server'
 
 import { connectRedis } from '@/lib/db'
+import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation'
 
 export async function createBook(formData) {
   const client = await connectRedis();
   const { title, rating, author, blurb } = Object.fromEntries(formData)
 
-  const id = Math.floor(Math.random() * 1000)
+  const id = Math.floor(Math.random() * 100000)
+
+  //Add the books to the sorted set (NX prevents duplicates)
+  const unique = await client.zAdd('books', {
+    value: title,
+    score: id
+  }, { NX: true })
+
+  if (!unique) {
+    return { error: 'This book already exists' }
+  }
+  //create a hash
   const body = {
     title,
     rating,
@@ -15,4 +27,7 @@ export async function createBook(formData) {
     blurb
   }
   await client.hSet(`book:${id}`, body)
+  revalidatePath('/')
+
+  return { success: true, redirectTo: '/' }
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,42 @@
+import { connectRedis } from '@/lib/db'
 import Link from 'next/link'
 
 const getBooks = async () => {
-  
+  const client = await connectRedis();
+  //Figuring out the keys for the hashes
+  const result = await client.zRangeWithScores('books', 0, -1)
+  //[{value: 'title', score: id}]
+
+  //Pipelines in redis
+  const books = Promise.all(
+    result.map((book) => {
+      return client.hGetAll(`book:${book.score}`)
+    })
+  )
+
+  return books
 }
 
 export default async function Home() {
-
+  const books = await getBooks()
   return (
     <main>
       <nav className="flex justify-between">
         <h1 className='font-bold'>Books on Redis!</h1>
         <Link href="/create" className="btn">Add a new book</Link>
       </nav>
-      
+
       <p>List of books here.</p>
+      <ul>
+        {books.map((book) => (
+          <div key={book.title} className='card'>
+            <h2>{book.title}</h2>
+            <p>Author: {book.author}</p>
+            <p>Rating: {book.rating}</p>
+            <p>Blurb: {book.blurb}</p>
+          </div>
+        ))}
+      </ul>
     </main>
   )
 }
